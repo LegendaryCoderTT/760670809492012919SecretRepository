bca4de030f72e480225eebba1f8d885d206051aa
Legendary Coder <csn2129@email.vccs.edu>
2016-08-08 11:55:57 -0700

    The Fucking Launcher didnt Commit
    fuckkkkk

--- start-game.bat
+++ start-game.bat
@@ -1,73 +1,26 @@
 @echo off
 
-title Toontown Stride Game Launcher
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
 
-echo Choose your connection method!
-echo.
-echo #1 - Localhost
-echo #2 - Dev Server
-echo #3 - Custom
-echo #4 - Local RemoteDB
-echo #5 - Prod Server
-echo.
+rem Get the user input:
+set /P ttsUsername="Username: "
+set /P ttsPassword="Password: "
 
-:selection
+rem Export the environment variables:
+set TTS_PLAYCOOKIE=%ttsUsername%:%ttsPassword%
+set TTS_GAMESERVER=127.0.0.1
 
-set INPUT=-1
-set /P INPUT=Selection: 
+cls
 
-if %INPUT%==1 (
-    set TTS_GAMESERVER=127.0.0.1
-) else if %INPUT%==2 (
-    set TTS_GAMESERVER=167.114.220.172
-) else if %INPUT%==4 (
-    set TTS_GAMESERVER=127.0.0.1
-) else if %INPUT%==5 (
-    SET TTS_GAMESERVER=lw2.ez-webz.com:7198
-) else if %INPUT%==3 (
-    echo.
-    set /P TTS_GAMESERVER=Gameserver: 
-) else (
-	goto selection
-)
+echo Contacting Toontown Dimensions Staff...
+cls
 
-echo.
+echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+echo Welcome to Toontown Dimensions, %ttsUsername%!
+echo The World of Toontown Awaits You!
+echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
 
-if %INPUT%==2 (
-    set /P ttsUsername="Username: "
-    set /P ttsPassword="Password: "
-) else if %INPUT%==4 (
-    set /P ttsUsername="Username: "
-    set /P ttsPassword="Password: "
-) else (
-    set /P TTS_PLAYCOOKIE=Username: 
-)
 
-echo.
-
-echo ===============================
-echo Starting Toontown Stride...
-echo ppython: "C:\Panda3D-1.10.0\python\ppython.exe"
-
-if %INPUT%==2 (
-    echo Username: %ttsUsername%
-) else if %INPUT%==4 (
-    echo Username: %ttsUsername%
-) else (
-    echo Username: %TTS_PLAYCOOKIE%
-)
-
-echo Gameserver: %TTS_GAMESERVER%
-echo ===============================
-
-:main
-if %INPUT%==2 (
-    "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStartRemoteDB
-) else if %INPUT%==4 (
-    "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStartRemoteDB
-) else (
-    "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStart
-)
+%PPYTHON_PATH% -m toontown.toonbase.ToontownDimensionsStart
 pause
-
-goto main
