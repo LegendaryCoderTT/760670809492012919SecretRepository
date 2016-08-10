a834239d7c48d4a521e125f26896ff9119b723c0
Legendary Coder <csn2129@email.vccs.edu>
2016-08-10 15:30:12 -0700

    Removed the Injector + added TTO Loading Screen
    no more tti stolen loading screen yay

--- toontown/toontowngui/ToontownLoadingScreen.py
+++ toontown/toontowngui/ToontownLoadingScreen.py
@@ -1,111 +1,68 @@
-from pandac.PandaModules import *
+# File: t (Python 2.4)
+
 from direct.gui.DirectGui import *
+from pandac.PandaModules import *
 from toontown.toonbase import ToontownGlobals
 from toontown.toonbase import TTLocalizer
-from toontown.hood import ZoneUtil
 import random
 
-
-LOADING_SCREEN_SORT_INDEX = 4000
-
-
 class ToontownLoadingScreen:
-
-    defaultTex = 'phase_3.5/maps/loading/default.jpg'
-    zone2picture = {
-        ToontownGlobals.GoofySpeedway : 'phase_3.5/maps/loading/gs.jpg',
-        ToontownGlobals.ToontownCentral : 'phase_3.5/maps/loading/ttc.jpg',
-        ToontownGlobals.SillyStreet : 'phase_3.5/maps/loading/ttc_ss.jpg',
-        ToontownGlobals.LoopyLane : 'phase_3.5/maps/loading/ttc_ll.jpg',
-        ToontownGlobals.PunchlinePlace : 'phase_3.5/maps/loading/ttc_pp.jpg',
-        ToontownGlobals.DonaldsDock : 'phase_3.5/maps/loading/dd.jpg',
-        ToontownGlobals.BarnacleBoulevard : 'phase_3.5/maps/loading/dd_bb.jpg',
-        ToontownGlobals.SeaweedStreet : 'phase_3.5/maps/loading/dd_ss.jpg',
-        ToontownGlobals.LighthouseLane : 'phase_3.5/maps/loading/dd_ll.jpg',
-        ToontownGlobals.DaisyGardens : 'phase_3.5/maps/loading/dg.jpg',
-        ToontownGlobals.ElmStreet : 'phase_3.5/maps/loading/dg_es.jpg',
-        ToontownGlobals.MapleStreet : 'phase_3.5/maps/loading/dg_ms.jpg',
-        ToontownGlobals.OakStreet : 'phase_3.5/maps/loading/dg_os.jpg',
-        ToontownGlobals.MinniesMelodyland : 'phase_3.5/maps/loading/mml.jpg',
-        ToontownGlobals.AltoAvenue : 'phase_3.5/maps/loading/mml_aa.jpg',
-        ToontownGlobals.BaritoneBoulevard : 'phase_3.5/maps/loading/mml_bb.jpg',
-        ToontownGlobals.TenorTerrace : 'phase_3.5/maps/loading/mml_tt.jpg',
-        ToontownGlobals.TheBrrrgh : 'phase_3.5/maps/loading/tb.jpg',
-        ToontownGlobals.WalrusWay : 'phase_3.5/maps/loading/tb_ww.jpg',
-        ToontownGlobals.SleetStreet : 'phase_3.5/maps/loading/tb_ss.jpg',
-        ToontownGlobals.PolarPlace : 'phase_3.5/maps/loading/tb_pp.jpg',
-        ToontownGlobals.DonaldsDreamland : 'phase_3.5/maps/loading/ddl.jpg',
-        ToontownGlobals.LullabyLane : 'phase_3.5/maps/loading/ddl_ll.jpg',
-        ToontownGlobals.PajamaPlace : 'phase_3.5/maps/loading/ddl_pp.jpg',
-        ToontownGlobals.OutdoorZone : 'phase_3.5/maps/loading/oz.jpg',
-        ToontownGlobals.GolfZone : 'phase_3.5/maps/loading/gz.jpg',
-        ToontownGlobals.SellbotHQ : 'phase_3.5/maps/loading/sbhq.jpg',
-        ToontownGlobals.CashbotHQ : 'phase_3.5/maps/loading/cbhq.jpg',
-        ToontownGlobals.LawbotHQ : 'phase_3.5/maps/loading/lbhq.jpg',
-        ToontownGlobals.BossbotHQ : 'phase_3.5/maps/loading/bbhq.jpg'
-    }
-
+    
     def __init__(self):
-        self.__expectedCount = 0
-        self.__count = 0
-        self.gui = loader.loadModel('phase_3/models/gui/progress-background.bam')
-        self.title = DirectLabel(guiId='ToontownLoadingScreenTitle', parent=self.gui, relief=None, pos=(0, 0, 0.24), text='', textMayChange=1, text_scale=0.1, text_fg=(1, 1, 2, 0.85), text_shadow=(0, 0, 0, 5), text_align=TextNode.ACenter, text_font=ToontownGlobals.getSignFont())
-        self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(base.a2dLeft+(base.a2dRight/4.95), base.a2dRight-(base.a2dRight/4.95), 0, -0.10), pos=(0, 0, 0.20), text='', frameColor=(1, 1, 1, 0.75), barColor=(1.0, 0, 0, 0.8))
-        self.waitBar.setTransparency(TransparencyAttrib.MAlpha)
-        logoScale = 0.5625  # Scale for our locked aspect ratio (2:1).
-        self.logo = OnscreenImage(
-            image='phase_3/maps/toontown-logo.png',
-            scale=(logoScale * 2.0, 1, logoScale))
-        self.logo.reparentTo(hidden)
-        self.logo.setTransparency(TransparencyAttrib.MAlpha)
-        scale = self.logo.getScale()
-        # self.logo.setPos(scale[0], 0, -scale[2])
-        self.logo.setPos(0, 0, -scale[2])
+        self._ToontownLoadingScreen__expectedCount = 0
+        self._ToontownLoadingScreen__count = 0
+        self.gui = loader.loadModel('phase_3/models/gui/progress-background')
+        self.banner = loader.loadModel('phase_3/models/gui/toon_council').find('**/scroll')
+        self.banner.reparentTo(self.gui)
+        self.banner.setScale(0.40000000000000002, 0.40000000000000002, 0.40000000000000002)
+        self.tip = DirectLabel(guiId = 'ToontownLoadingScreenTip', parent = self.banner, relief = None, text = '', text_scale = TTLocalizer.TLStip, textMayChange = 1, pos = (-1.2, 0.0, 0.10000000000000001), text_fg = (0.40000000000000002, 0.29999999999999999, 0.20000000000000001, 1), text_wordwrap = 13, text_align = TextNode.ALeft)
+        self.title = DirectLabel(guiId = 'ToontownLoadingScreenTitle', parent = self.gui, relief = None, pos = (-1.0600000000000001, 0, -0.77000000000000002), text = '', textMayChange = 1, text_scale = 0.080000000000000002, text_fg = (0, 0, 0.5, 1), text_align = TextNode.ALeft)
+        self.waitBar = DirectWaitBar(guiId = 'ToontownLoadingScreenWaitBar', parent = self.gui, frameSize = (-1.0600000000000001, 1.0600000000000001, -0.029999999999999999, 0.029999999999999999), pos = (0, 0, -0.84999999999999998), text = '')
 
+    
     def destroy(self):
+        self.tip.destroy()
         self.title.destroy()
+        self.waitBar.destroy()
+        self.banner.removeNode()
         self.gui.removeNode()
-        self.logo.removeNode()
 
+    
     def getTip(self, tipCategory):
         return TTLocalizer.TipTitle + '\n' + random.choice(TTLocalizer.TipDict.get(tipCategory))
 
-    def begin(self, range, label, gui, tipCategory, zoneId):
+    
+    def begin(self, range, label, gui, tipCategory):
         self.waitBar['range'] = range
         self.title['text'] = label
-        if ToontownGlobals.BossbotHQ <= zoneId <= ToontownGlobals.LawbotHQ:
-            self.title['text_font'] = ToontownGlobals.getSuitFont()
-        else:
-            self.title['text_font'] = ToontownGlobals.getSignFont()
-        loadingScreenTex = self.zone2picture.get(ZoneUtil.getBranchZone(zoneId), self.defaultTex)
-        self.background = loader.loadTexture(loadingScreenTex)
-        self.__count = 0
-        self.__expectedCount = range
+        self.tip['text'] = self.getTip(tipCategory)
+        self._ToontownLoadingScreen__count = 0
+        self._ToontownLoadingScreen__expectedCount = range
         if gui:
-            self.title.setPos(0, 0, 0.26)
-            self.gui.setPos(0, -0.1, 0)
-            self.gui.reparentTo(aspect2d, LOADING_SCREEN_SORT_INDEX)
-            self.gui.setTexture(self.background, 1)
-            if loadingScreenTex == self.defaultTex:
-               self.logo.reparentTo(base.a2dpTopCenter, LOADING_SCREEN_SORT_INDEX)
+            self.waitBar.reparentTo(self.gui)
+            self.title.reparentTo(self.gui)
+            self.gui.reparentTo(aspect2d)
         else:
+            self.waitBar.reparentTo(aspect2d)
+            self.title.reparentTo(aspect2d)
             self.gui.reparentTo(hidden)
-            self.logo.reparentTo(hidden)
-        self.title.reparentTo(base.a2dpBottomCenter, LOADING_SCREEN_SORT_INDEX)
-        self.waitBar.reparentTo(base.a2dpBottomCenter, LOADING_SCREEN_SORT_INDEX)
-        self.waitBar.update(self.__count)
+        self.waitBar.update(self._ToontownLoadingScreen__count)
 
+    
     def end(self):
         self.waitBar.finish()
         self.waitBar.reparentTo(self.gui)
         self.title.reparentTo(self.gui)
         self.gui.reparentTo(hidden)
-        self.logo.reparentTo(hidden)
-        return (self.__expectedCount, self.__count)
+        return (self._ToontownLoadingScreen__expectedCount, self._ToontownLoadingScreen__count)
 
+    
     def abort(self):
         self.gui.reparentTo(hidden)
 
+    
     def tick(self):
-        self.__count = self.__count + 1
-        self.waitBar.update(self.__count)
+        self._ToontownLoadingScreen__count = self._ToontownLoadingScreen__count + 1
+        self.waitBar.update(self._ToontownLoadingScreen__count)
+
+
